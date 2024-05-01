93e1cfcd3a41f6fe4b6ecfc2721aba126fbafe55
Maksim101996 <akakii.botov@mail.com>
2024-05-02 03:06:46 +0500

    #3 Найти сторонние апи #8 Определиться со стилями(module and scss) #5 Создать шаблон приложения, частично. ( обновлена файловая структура, установлен роутинг)

---
+++ frontend/src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import styles from "./Header.module.scss"
+
+const Header: FC = () => {
+	return <div className={styles.header}>
+		header
+	</div>
+}
+
+export default Header
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 export const API = {
-    v1: axios.create({
-        baseURL: process.env.REACT_APP_API || 'http://localhost:6000error'
-    })
+	v1: axios.create({
+		baseURL: process.env.REACT_APP_API || 'http://localhost:6000error'
+	})
 }
 
+
+
+
+// base url:    https://api.coingecko.com/api/v3/
+// /coins/categories/list endpoint для запроса всех категорий монет
+//  /simple/token_price/{id}   endpoint для запроса цены монеты
+//  /coins/{id}  endpoint для запроса всех данных монеты (название, цена, рынок)
+//  /coins/{id}/history  endpoint для запроса исторических данных(цена, капитализация, объём)
+// /coins-id-ohlc endpoint для получения графика по монете
+
+// По мере надобности буду дополнять апишку
