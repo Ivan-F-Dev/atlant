103ee44ee7219ec196c1559af827ea4b16737821
LAPTOP-I91SDHHB\admin <ivanfomichev22@mail.ru>
2024-05-03 18:20:57 +0500

    #4  Написать авторизацию

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import cookie from 'cookie-parser'
 
 dotenv.config()
 
-const app = express()
-app.use(cors())
-const port = process.env.port || 5000
-app.get('/', (request, response) => {
-    response.send('Hello world!')
-});
-app.get('/todo', (request, response) => {
-    response.json({
-        url: 'todo'
-    })
-});
-app.listen(port, () => console.log(`Running on port ${port}`))
+const start = () => {
+    try {
+        const app = express()
+        app.use(cors())
+        const port = process.env.port || 5000
+        app.get('/', (request, response) => {
+            response.send('Hello world!')
+        });
+        app.get('/todo', (request, response) => {
+            response.json({
+                url: 'todo'
+            })
+        });
+        app.listen(port, () => console.log(`Running on port ${port}`))
+    } catch (e) {
+        console.log("Ошибка!",e)
+    }
+}
+
